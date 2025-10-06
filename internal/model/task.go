804f06db59374842f014534390b8efab8242925c
g-lalioti <giorgoslaliotis@gmail.com>
2025-10-06 21:51:30 +0300

    updating task.go

--- internal/model/task.go
+++ internal/model/task.go
@@ -6,5 +6,6 @@ type Task struct {
 	ID        uint      `json:"id" gorm:"primaryKey"`
 	Title     string    `json:"title"`
 	Completed bool      `json:"completed"`
+	UserID    uint      `json:"user_id"` // 🔗 Foreign key
 	CreatedAt time.Time `json:"created_at" gorm:"autoCreateTime"`
 }
