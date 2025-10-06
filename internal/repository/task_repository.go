c7a3c4641c40f6dcae1e388025186617d70fe769
g-lalioti <giorgoslaliotis@gmail.com>
2025-10-06 21:52:56 +0300

    task_repository adding  multi user

--- internal/repository/task_repository.go
+++ internal/repository/task_repository.go
@@ -7,9 +7,9 @@ import (
 
 type TaskRepository interface {
 	Create(task *model.Task) error
-	GetAll() ([]model.Task, error)
+	GetAll(userID *uint) ([]model.Task, error)
 	Update(task *model.Task) error
-	Delete(id uint) error
+	Delete(id uint, userID *uint) error
 }
 
 type taskRepository struct {
@@ -20,20 +20,34 @@ func NewTaskRepository(db *gorm.DB) TaskRepository {
 	return &taskRepository{db: db}
 }
 
+// Create a new task
 func (r *taskRepository) Create(task *model.Task) error {
 	return r.db.Create(task).Error
 }
 
-func (r *taskRepository) GetAll() ([]model.Task, error) {
+// GetAll returns all tasks (optionally filtered by userID)
+func (r *taskRepository) GetAll(userID *uint) ([]model.Task, error) {
 	var tasks []model.Task
-	err := r.db.Find(&tasks).Error
+	query := r.db
+
+	if userID != nil {
+		query = query.Where("user_id = ?", *userID)
+	}
+
+	err := query.Find(&tasks).Error
 	return tasks, err
 }
 
+// Update an existing task
 func (r *taskRepository) Update(task *model.Task) error {
 	return r.db.Save(task).Error
 }
 
-func (r *taskRepository) Delete(id uint) error {
-	return r.db.Delete(&model.Task{}, id).Error
+// Delete a task by ID (and userID if provided)
+func (r *taskRepository) Delete(id uint, userID *uint) error {
+	query := r.db.Model(&model.Task{}).Where("id = ?", id)
+	if userID != nil {
+		query = query.Where("user_id = ?", *userID)
+	}
+	return query.Delete(&model.Task{}).Error
 }
